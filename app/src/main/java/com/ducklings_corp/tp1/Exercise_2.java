7da7c5b978f39282f4faa52e5d11b0bf92c42d92
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2019-04-04 12:10:49 -0300

    Finished main menu, ex1 and start ex2

--- app/src/main/java/com/ducklings_corp/tp1/Exercise_2.java
+++ app/src/main/java/com/ducklings_corp/tp1/Exercise_2.java
@@ -2,6 +2,8 @@ package com.ducklings_corp.tp1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class Exercise_2 extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class Exercise_2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exercise_2);
+    }
+
+    public void processAsText(View view) {
+        String givenText;
+        EditText inputController;
+
+        inputController = findViewById(R.id.ex3_textInput);
+        givenText = inputController.getText().toString();
+
+
     }
 }
--- app/src/main/java/com/ducklings_corp/tp1/MainActivity.java
+++ app/src/main/java/com/ducklings_corp/tp1/MainActivity.java
@@ -1,7 +1,9 @@
 package com.ducklings_corp.tp1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goAct(View view) {
+        int id;
+        Intent destination;
+
+        destination = null;
+
+        id  = view.getId();
+        switch(id) {
+            case R.id.goAct1:
+                destination = new Intent(MainActivity.this,Exercise_1.class);
+                break;
+            case R.id.goAct2:
+                destination = new Intent(MainActivity.this,Exercise_2.class);
+                break;
+            default:
+                return;
+        }
+        startActivity(destination);
     }
 }
